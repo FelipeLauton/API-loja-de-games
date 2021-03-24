aacb93753eb5ee6e8b19bcc33b57595c0dadcf6a
unknown <felipelauton021@gmail.com>
2021-03-24 10:58:37 -0300

    API com as duas tabelas para a loja de games

---
+++ LojaDeGames/src/main/java/org/generation/LojaDeGames/model/Produto.java
@@ -0,0 +1,79 @@
+package org.generation.LojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+	//criando as tabelas do banco
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 10, max = 200)
+	private String descricao;
+	
+	@NotNull
+	private double preco;
+	
+	@NotNull
+	private long estoque;
+	
+	//relacionando tabelas
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	//Encapsulamento
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public long getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(long estoque) {
+		this.estoque = estoque;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
